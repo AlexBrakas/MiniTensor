a0b00c484938d55d2bdf4f3b67bdfddbc0090ff9
Alex Brakas <abrakas@outlook.com>
2025-10-01 00:14:25 -0400

    Inital commit

---
+++ src/MiniTensor.cpp
@@ -0,0 +1,12 @@
+﻿// NeuralNetwork.cpp : Defines the entry point for the application.
+//
+
+#include "MiniTensor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
